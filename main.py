bd7ead4f10066427417082aefdd07119672fc6ce
Kevin <neoglitch822@yahoo.com>
2024-01-30 15:34:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -101,4 +101,7 @@ def main():
             break
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}")
 
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
+
 main()
